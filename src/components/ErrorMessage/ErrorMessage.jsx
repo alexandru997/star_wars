d0f987ff430686ab38ad3e09151458a90f05f1f4
alexandru997 <52069086+alexandru997@users.noreply.github.com>
2021-11-10 17:08:58 +0200

    commit number 6

---
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./ErrorMessage.module.css";
+function ErrorMessage() {
+  return (
+    <div>
+      <p className={styles.text}>
+        The dark side of the force has won. <br />
+        We cannot display data.
+        <br />
+        Come back when we fix everything
+      </p>
+    </div>
+  );
+}
+
+export default ErrorMessage;
--- src/components/peoplePage/PeoplePage.jsx
+++ src/components/peoplePage/PeoplePage.jsx
@@ -1,26 +1,40 @@
 import React, { useEffect, useState } from "react";
+import { withErrorApi } from "../../hoc-helpers/withErrorApi";
+
 import { getApiResurce } from "../../utils/network";
 import { API_PEOPLE } from "../../constants/api";
 import { getPeopleId, getPeopleImg } from "../../services/getPeopleData";
 import PeopleList from "./poepleList/PeopleList";
 
 // import styles from "./PeoplePage.module.css";
-function PeoplePage() {
+function PeoplePage({ setErrorApi }) {
   const [people, setPeople] = useState(null);
 
   const getResource = async (url) => {
     const res = await getApiResurce(url);
-    const peopleList = res.results.map(({ name, url }) => {
-      const id = getPeopleId(url);
-      const img = getPeopleImg(id);
-      return { id, name, img };
-    });
-    setPeople(peopleList);
+
+    if (res) {
+      const peopleList = res.results.map(({ name, url }) => {
+        const id = getPeopleId(url);
+        const img = getPeopleImg(id);
+        return { id, name, img };
+      });
+      setPeople(peopleList);
+      setErrorApi(false);
+    } else {
+      setErrorApi(true);
+    }
   };
   useEffect(() => {
     getResource(API_PEOPLE);
   }, []);
-  return <div>{people && <PeopleList people={people} />}</div>;
+  return (
+    <div>
+      <h1>Navigation</h1>
+
+      {people && <PeopleList people={people} />}
+    </div>
+  );
 }
 
-export default PeoplePage;
+export default withErrorApi(PeoplePage);
